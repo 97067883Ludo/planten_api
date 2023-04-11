9722c31542f2ee4e81c40dd20a7342fa456cffd3
ludotielbeke <ludotielbeke@gmail.com>
2023-04-11 10:44:23 +0200

    wired stuff with the id but it is working for now... still looking into a better solution

--- planten_api/Controllers/SoilMoistureController.cs
+++ planten_api/Controllers/SoilMoistureController.cs
@@ -24,10 +24,15 @@ public class SoilMoistureController : ControllerBase
     [HttpPost]
     public IActionResult Post(SoilMoisture soilMoisture)
     {
+        int ID = (from i in _db.SoilMoistures
+                  select i.SoilMoistureId).Max();
+        
+        soilMoisture.SoilMoistureId = ID + 1;
+        
         soilMoisture.createdAt = DateTime.Now;
         
         _db.SoilMoistures.Add(soilMoisture);
-            
+        
         _db.SaveChanges();
 
         return Ok();
--- planten_api/SoilMoisture.cs
+++ planten_api/SoilMoisture.cs
@@ -3,6 +3,7 @@ namespace planten_api;
 public class SoilMoisture
 {
     public int SoilMoistureId { get; set; }
+    
     public int Moisture { get; set; }
     public DateTime? createdAt { get; set; }
 }
