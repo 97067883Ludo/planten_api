2a9a3cccff34b1edbaca55155ca6cd15052987d3
ludotielbeke <ludotielbeke@gmail.com>
2023-04-13 09:24:16 +0200

    push for test

--- planten_api/Controllers/SoilMoistureController.cs
+++ planten_api/Controllers/SoilMoistureController.cs
@@ -26,11 +26,13 @@ public class SoilMoistureController : ControllerBase
 
     [HttpPost]
     public IActionResult Post(SoilMoisture soilMoisture)
-    {
-        int ID = (from i in _db.SoilMoistures
+    { 
+        /*int ID = (from i in _db.SoilMoistures
                 select i.SoilMoistureId).Max();
         
-        soilMoisture.SoilMoistureId = ID + 1;
+        soilMoisture.SoilMoistureId = ID + 1;*/
+        
+        Console.WriteLine(soilMoisture.Id);
         
         soilMoisture.createdAt = DateTime.Now;
         
--- planten_api/SoilMoisture.cs
+++ planten_api/SoilMoisture.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace planten_api;
 
-public class SoilMoisture
+public class SoilMoisture : DbContext
 {
-    public int SoilMoistureId { get; set; }
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
     
     public int Moisture { get; set; }
     public DateTime? createdAt { get; set; }
